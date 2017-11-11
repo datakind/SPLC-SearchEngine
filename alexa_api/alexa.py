83a64fa5352b178758a142e59e7a2ca05cd3f312
Vito Colano <vcolano@gmail.com>
2017-11-11 18:48:19 -0500

    added response dictionary parsing for url_info endpoint request method

--- alexa_api/alexa.py
+++ alexa_api/alexa.py
@@ -20,34 +20,63 @@ __email__ = "ruby.w.chu@gmail.com, vcolano@gmail.com"
 import awis
 import getpass
 import lxml
+from json import loads, dumps
 import pprint
 import pickle
 import os
+import xmltodict
+from collections import defaultdict
 
 class Alexa():
 
     ACCESS_ID = "AKIAIIKNPTNCGEICYBIQ"
     URL_INFO_RESPONSE_PARAMETERS = ['RelatedLinks', 'Categories', 'Rank', 'RankByCountry', 'UsageStats', 'AdultContent',
                                     'Speed', 'Language', 'OwnedDomains', 'LinksInCount', 'SiteData']
+    NAMESPACES = {
+        'http://alexa.amazonaws.com/doc/2005-10-05/': None,
+        'http://awis.amazonaws.com/doc/2005-07-11': None
+    }
 
     def __init__(self):
-        self.secret = getpass.getpass()
+        self.secret = getpass.getpass(prompt='AWS Secret Key:')
         self.api = awis.AwisApi(Alexa.ACCESS_ID, self.secret)
 
-    def url_info(self):
-        return self.api.url_info("http://www.breitbart.com/", *Alexa.URL_INFO_RESPONSE_PARAMETERS)
-
-    @staticmethod
-    def etree_to_dict(t):
-        d = {t.tag: map(Alexa.etree_to_dict, t.iterchildren())}
-        d.update(('@' + k, v) for k, v in t.attrib.iteritems())
-        d['text'] = t.text
-        return d
+    def url_info(self, url):
+        xml_response = self.api.url_info(url, *Alexa.URL_INFO_RESPONSE_PARAMETERS, as_xml=False)
+        res_dict = loads(dumps(xmltodict.parse(xml_response, process_namespaces=True, namespaces=Alexa.NAMESPACES)))
+        flat_dict = {}
+        if not res_dict['UrlInfoResponse']['Response']['ResponseStatus']['StatusCode'] == 'Success':
+            print("Error, unsuccessful response from api for the following url:\t" + url)
+            return None
+        elif 'Alexa' in res_dict['UrlInfoResponse']['Response']['UrlInfoResult']:
+            if 'ContentData' in res_dict['UrlInfoResponse']['Response']['UrlInfoResult']['Alexa']:
+                content = res_dict['UrlInfoResponse']['Response']['UrlInfoResult']['Alexa']['ContentData']
+                flat_dict['ExternalLinksToSite'] = content.get('LinksInCount')
+                flat_dict['OwnersOtherDomains'] = content.get('OwnedDomains')
+                if 'SiteData' in content:
+                    flat_dict['SiteDescription'] = content['SiteData'].get('Description')
+                    flat_dict['OnlineSince'] = content['SiteData'].get('OnlineSince')
+                    flat_dict['SiteTitle'] = content['SiteData'].get('Title')
+            if 'TrafficData' in res_dict['UrlInfoResponse']['Response']['UrlInfoResult']['Alexa']:
+                traffic = res_dict['UrlInfoResponse']['Response']['UrlInfoResult']['Alexa']['TrafficData']
+                flat_dict['ThreeMonthAvgUSRank'] = traffic.get('Rank')
+                if 'UsageStatistics' in traffic and traffic['UsageStatistics']['UsageStatistic']:
+                    if 'PageViews' in traffic['UsageStatistics']['UsageStatistic'][0]:
+                        pageviews = traffic['UsageStatistics']['UsageStatistic'][0]['PageViews']
+                        if 'PerMillion' in pageviews:
+                            flat_dict['PageViewsPerMillion'] = pageviews['PerMillion'].get('Value')
+                        if 'PerUser' in pageviews:
+                            flat_dict['PageViewsPerUser'] = pageviews['PerUser'].get('Value')
+                if 'ContributingSubdomains' in traffic and traffic['ContributingSubdomains']['ContributingSubdomain']:
+                    subdomains = ''
+                    for subdomain in traffic['ContributingSubdomains']['ContributingSubdomain']:
+                        subdomains += subdomain.get('DataUrl') + ', '
+                    flat_dict['ContributingSubdomains'] = subdomains.strip(', ')
+        return flat_dict
 
 a = Alexa()
-res = a.url_info()
-d = Alexa.etree_to_dict(res)
+res = a.url_info("http://www.breitbart.com/")
 
 f = open('/home/vito/projects/SPLC-SearchEngine/alexa_api/example_lxml_obj.txt', 'wb')
-pickle.dump(d, f)
+pickle.dump(res, f)
 f.close()
