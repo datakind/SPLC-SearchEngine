a614e7f1fdd2c61b60c5585a6d3627eb6c6e2f6c
Nils Feege <nils.feege@stonybrook.edu>
2017-11-11 16:59:15 -0500

    Add cleanup of text fields

--- api_4chan/extract_4chan_comments.py
+++ api_4chan/extract_4chan_comments.py
@@ -6,12 +6,32 @@
 # - basc-py4chan
 # - progressbar2
 
+# @TODO's:
+# * Extract URL information (base URL)
+# * Extract user information?
+# * flag main post- combine comments?
+
 # import libraries
 import sys
 import pandas as pd
 
 import basc_py4chan
 from progressbar import ProgressBar
+
+# clean up function
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation) 
+lemma = WordNetLemmatizer()
+
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
 
 def main():
 
@@ -36,6 +56,7 @@ def main():
             'Thread',
             'Sticky?',
             'Closed?',
+            'Posts_in_thread',
             'Topic Post',
             'Postnumber',
             'Timestamp',
@@ -55,7 +76,7 @@ def main():
     pbar = ProgressBar()
 
     # loop over all threads
-    for thread_id in pbar( thread_ids ) :
+    for thread_id in pbar( thread_ids[:5] ) :
         thread = board.get_thread( thread_id )
 
         # topic information - loop oever all topics
@@ -68,17 +89,27 @@ def main():
         # if all_posts found, extract all comments
         for post in all_posts:
 
+            # clean up text in tsubject field
+            if ( post.subject ):
+                subject_clean = clean(post.subject)
+            else:
+                subject_clean = 'N/A'
+
+            # clean up text in text_comment field
+            comment_clean = clean(post.text_comment)
+
             # append to dataframe
             df.loc[len(df)] = [
                 thread,
                 thread.sticky,
                 thread.closed,
+                len(all_posts),
                 post,
                 post.post_number,
                 post.timestamp,
                 repr(post.datetime),
-                post.subject,
-                post.text_comment,
+                subject_clean,
+                comment_clean,
                 post.file
                 ]
 
