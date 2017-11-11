6a403d23883befd59d06f93bdcf86b5ce3115612
Nils Feege <nils.feege@stonybrook.edu>
2017-11-11 14:48:30 -0500

    Loop over all comments and add progress bar

--- api_4chan/extract_4chan_comments.py
+++ api_4chan/extract_4chan_comments.py
@@ -1,13 +1,17 @@
 # Python macro using the 4chan API to access 4chan board and extracting infromation from threads and comments into
 # a CSV file.
 
-# import libraries
-# if basc_4chan library is missing, install with pip3 basc-py4chan
-import basc_py4chan
+# check that these libraries are installed- install (using pip) if not:
+# - pandas
+# - basc-py4chan
+# - progressbar2
 
-# import more libraries
+# import libraries
 import sys
 import pandas as pd
+
+import basc_py4chan
+from progressbar import ProgressBar
 
 def main():
 
@@ -46,41 +50,40 @@ def main():
     str_thread_ids = [str(id) for id in thread_ids]  # need to do this so str.join below works
     print('There are', len(thread_ids), 'active threads on /',boardname,'/:', ', '.join(str_thread_ids))
 
-    # grab a thread (@TODO: Loop over all threads)
-    thread = board.get_thread(148985682)
+    # prepare progress bar
+    print( 'Progress:' )
+    pbar = ProgressBar()
 
-    # thread information
-    print('Thread',  thread)
-    print('Sticky?', thread.sticky)
-    print('Closed?', thread.closed)
+    # loop over all threads
+    for thread_id in pbar( thread_ids ) :
+        thread = board.get_thread( thread_id )
 
-    # topic information - loop oever all topics
-    all_posts = thread.all_posts
-    for post in all_posts:
-         print('Topic Post', post)
-         print('Postnumber', post.post_number)
-         print('Timestamp',  post.timestamp)
-         print('Datetime',   repr(post.datetime))
-         print('Subject',    post.subject)
-         print('Comment',    post.text_comment)
-         print('File',    post.file)
+        # topic information - loop oever all topics
+        all_posts = thread.all_posts
 
-         # append to dataframe
-         df.loc[len(df)] = [
-             thread,
-             thread.sticky,
-             thread.closed,
-             post,
-             post.post_number,
-             post.timestamp,
-             repr(post.datetime),
-             post.subject,
-             post.text_comment,
-             post.file
-             ]
+        # skip if all_posts not found
+        if ( all_posts == None ):
+            continue
+
+        # if all_posts found, extract all comments
+        for post in all_posts:
+
+            # append to dataframe
+            df.loc[len(df)] = [
+                thread,
+                thread.sticky,
+                thread.closed,
+                post,
+                post.post_number,
+                post.timestamp,
+                repr(post.datetime),
+                post.subject,
+                post.text_comment,
+                post.file
+                ]
 
     # print head of created dataframe
-    print(df.head())
+    #print(df.head())
 
     # write dataframe to CSV output file
     df.to_csv( path_or_buf = output_csv )
