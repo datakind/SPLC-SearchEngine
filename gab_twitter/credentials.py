b97eb8e85458f0c01e908c2fc248b3d31ca225fb
David Turbay <dturbay@twitter.com>
2017-11-11 21:07:06 -0500

    clean creds

--- gab_twitter/credentials.py
+++ gab_twitter/credentials.py
@@ -6,8 +6,8 @@ login_payload = {
 }
 
 TWITTER_API_KEYS = {
-    'consumer_key': 'X1qoKEBhm25H4DUKZStSMMJef',
-    'consumer_secret': 'HoB4a1ezxkbtXRuhp9N1V0l9F5vQl2PIhozkn3HosCuKVFEzlP' ,
-    'access_token_key': '331257610-4OawcSoJVbADVhXsHARst6zd8CmaII4e0lBuUdcA' ,
-    'access_token_secret': 'NZsbIrLt53ohP7toigE33IrzJ8H9jOeeyesOZ4WDu46eC',
+    'consumer_key': '',
+    'consumer_secret': '' ,
+    'access_token_key': '' ,
+    'access_token_secret': '',
 }
